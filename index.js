33b1c746e2f8825b6bae67eebbf5888b1c2bafc5
Cesar Castillo <cesarcast1llo@yahoo.com>
2017-10-23 18:50:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+  var node = document.getElementById('grand-node')
+  var kind = node.children[0]
+  while (kind) {
+    node = kind
+    kind = node.children[0]
+  }
+  return node;
+}
+
+function increaseRankBy(num) {
+  var rank = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0; i < rank.length; i++) {
+    var kid = rank[i].children
+
+    for (let k = 0; k < kid.length; k++) {
+      kid[k].innerHTML = parseInt(kid[k].innerHTML) + num
+    }
+  }
+}
