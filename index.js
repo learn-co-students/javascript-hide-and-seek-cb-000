13ab2986fba5112423b332cc1c398a4e07e540b5
cptbrkfst <dyl@resonategroup.ca>
2017-12-08 15:33:29 -0500

    done lol

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 var getFirstSelector = selector => {
-  return document.querySelector(selector);
-};
+  return document.querySelector(selector)
+}
 
 var nestedTarget = () => {
-  return document.querySelector("#nested .target");
-};
+  return document.querySelector('#nested .target')
+}
 
-var increaseRankBy = n => {
-  const uls = document.getElementById("app").querySelectorAll("ul li");
-  uls.forEach(li => {
-    li.innerHTML = (li + n).toString;
-  });
-};
+function deepestChild() {
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
 
-var deepestChild = () => {};
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+
+  return node
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
+}
