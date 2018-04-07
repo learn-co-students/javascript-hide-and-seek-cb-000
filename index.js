67147de4438f9ad0c1992dc95121ac2e302275dc
Chris Nunez <cenunez47@gmail.com>
2018-04-07 21:08:10 +0000

    Done.

--- index.js
+++ index.js
@@ -7,24 +7,24 @@ function nestedTarget() {
 }
 
 function deepestChild() {
-  var node = document.getElementById('grand-node') //node is being defined as the ID 'grand-node'
-  var kind = node.children[0]                      //kind is being defined as the first child element of node[in this case, 'grand-node']
+  var node = document.getElementById('grand-node'); //node is being defined as the ID 'grand-node'
+  var kind = node.children[0];                      //kind is being defined as the first child element of node[in this case, 'grand-node']
 
   while (kind) {            //while there is a kind variable
-    node = kind             //kind becomes the parent element 'grand-node'
-    kind = node.children[0] //the first child of the new 'grand-node' is selected
+    node = kind;            //kind becomes the parent element 'grand-node'
+    kind = node.children[0]; //the first child of the new 'grand-node' is selected
   }                         //this is repeated until there is no longer a kind variable
   return node;
 }
 
 function increaseRankBy(num) {
-  var rank = document.querySelectorAll('.ranked-list')//rank is all elements with a class of 'ranked-list'
+  var rank = document.querySelectorAll('.ranked-list'); //rank is all elements with a class of 'ranked-list'
 
   for (let i = 0; i < rank.length; i++) {             //loop through each ranked-list and define its children as kid
-    var kid = rank[i].children
+    var kid = rank[i].children;
 
     for (let k = 0; k < kid.length; k++) {            //loop through each kid
-      kid[k].innerHTML = parseInt(kid[k].innerHTML) + num //grab the text of each kid and parse it into an integer then += the num argument
+      kid[k].innerHTML = parseInt(kid[k].innerHTML) + num; //grab the text of each kid and parse it into an integer then += the num argument
     }
   }
 }
