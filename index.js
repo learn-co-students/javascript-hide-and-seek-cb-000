e986c7867b4831bcfe0dd7d1f90ef80a7d825b47
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-19 23:06:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+
+}
+
+function increaseRankBy(n) {
+  var rn = parseInt(n, 10) ;
+  var items = document.querySelectorAll(' .ranked-list li') ;
+  for (let i = 0 ; i < items.length ; i++) {
+    items[i].textContent = parseInt(items[i].textContent, 10) + rn
+  }
+}
+
+function deepestChild() {
+  var kid = document.querySelector(' #grand-node div div div div')
+  return kid
+}
